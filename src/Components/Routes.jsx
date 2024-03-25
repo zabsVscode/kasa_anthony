910f4cee418ebc800219eb2bacde962b130f8353
zabsVscode <anthonyguiselin62@gmail.com>
2024-03-25 14:50:12 +0100

    kasa_Anthony

---
+++ src/Components/Routes.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Card from "../Pages/Card";
+import ErrorPage from "../Pages/ErrorPage";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logement" element={<Card />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
